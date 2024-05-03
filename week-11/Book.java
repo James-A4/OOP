4071a841e4067725575b7ea114d31a1360e861a1
James-A4 <158222127+James-A4@users.noreply.github.com>
2024-05-03 04:10:07 +0000

    "Book.java"

--- week-11/Book.java
+++ week-11/Book.java
@@ -1,15 +1,49 @@
 public class Book {
+    private String title;
+    private String author;
+    private String content;
+    private int edition;
 
     public Book(String t, String a, String c, int e) {
 
-        public getPages() {
 
-            return pages;
-
-            
-        }
-
-        
+        this.title = t;
+        this.author = a;
+        this.content = c;
+        this.edition = e;
     }
+
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getEdition() {
+        return edition;
+    }
+
+    public int getPages() {
+        int contentLength = content.length();
+        int pages = contentLength / 700;
+        if (contentLength % 700 != 0) {
+            pages++;
     
+        }
+        return pages;
+    }
+
+
+    public String toString() {
+        return "Title: " + title + "\nAuthor: " + author + "\nEdition: " + edition + "\n";
+    }
 }
