ecb6e6f854bf582b24fdc4d4a7b8618e363eb2dd
James-A4 <158222127+James-A4@users.noreply.github.com>
2024-05-04 10:21:46 +0000

    "Vending machine progress"

--- week-11/VendingMachine.java
+++ week-11/VendingMachine.java
@@ -1,8 +1,8 @@
-import java.util.List;
-import java.util.Set;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class VendingMachine {
     private List<Book> shelf;
@@ -19,9 +19,14 @@ public class VendingMachine {
         this.password = password;
     }
 
+
+
+
     public int getCassette() {
         return this.cassette;
     }
+
+
 
     public void insertCoin(int coin) {
         Set<Integer> validDenominations = new HashSet<>(Arrays.asList(1, 2, 5, 10, 20, 50, 100, 200));
@@ -39,11 +44,12 @@ public class VendingMachine {
     }
 
 
+
     public void restock(List<Book> books, String password) {
         if (this.password.equals(password)) {
             this.shelf.addAll(books);
         } else {
-            throw new InvalidPasswordException("Invalid password for restocking");
+            throw new InvalidPasswordException("The provided password is invalaid for restocking books");
         }
     }
 
@@ -53,7 +59,7 @@ public class VendingMachine {
             this.safe = 0;
             return oldSafe;
         } else {
-            throw new InvalidPasswordException("Invalid password for emptying safe");
+            throw new InvalidPasswordException("The provided password is invalaid for emptying the safe");
         }
     }
 
@@ -67,7 +73,7 @@ public class VendingMachine {
 
     public int getPrice(int index) {
         if (index < 0 || index >= this.shelf.size()) {
-            throw new IndexOutOfBoundsException("Invalid book index: " + index);
+            throw new IndexOutOfBoundsException("Book index invalid: " + index);
         } else {
             Book book = this.shelf.get(index);
             int price = (int) Math.ceil(book.getPages() * this.locationFactor);
@@ -77,7 +83,7 @@ public class VendingMachine {
 
     public Book buyBook(int index) {
         if (index < 0 || index >= this.shelf.size()) {
-            throw new IndexOutOfBoundsException("Invalid book index: " + index);
+            throw new IndexOutOfBoundsException("Book index invalid: " + index);
         } else {
             Book book = this.shelf.get(index);
             int price = (int) Math.ceil(book.getPages() * this.locationFactor);
@@ -87,7 +93,7 @@ public class VendingMachine {
                 this.safe += price;
                 return book;
             } else {
-                throw new CassetteException("Not enough money in cassette to buy book");
+                throw new CassetteException("Insufficient funds in cassette to purchase book");
             }
         }
     }
