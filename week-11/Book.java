d5ce92b45ab93014099ec1b4b7db27a88d9cea27
James-A4 <158222127+James-A4@users.noreply.github.com>
2024-05-04 01:49:07 +0000

    "Book.java progress"

--- week-11/Book.java
+++ week-11/Book.java
@@ -4,9 +4,10 @@ public class Book {
     private String content;
     private int edition;
 
+
+
+
     public Book(String t, String a, String c, int e) {
-
-
         this.title = t;
         this.author = a;
         this.content = c;
@@ -15,31 +16,26 @@ public class Book {
 
 
 
-
     public String getTitle() {
         return title;
     }
-
     public String getAuthor() {
         return author;
     }
-
     public String getContent() {
         return content;
     }
-
     public int getEdition() {
         return edition;
     }
 
+
+
     public int getPages() {
         double contentLength = content.length();
-        return (int) Math.ceil(contentLength / 700);
+        return (int) Math.ceil(contentLength / 750.0); // Changed from 700 to 750 as per the requirements
     }
-
-}
-
-
+    @Override
     public String toString() {
         return "Title: " + title + "\nAuthor: " + author + "\nEdition: " + edition + "\n";
     }
